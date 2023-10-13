a71f544c69dcbc3c70ac1bad9eac228fa6abbcab
JayO <BlueJayVRStudio@gmail.com>
2023-10-13 13:55:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,35 +10,58 @@ from threading import Thread
 
 # app = Flask(__name__)
 
-def socketThread():
-    # Create a socket object
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# def socketThread():
+#     # Create a socket object
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Bind the socket to a specific network interface and port number
-    s.bind(('147.182.204.67', 5000))
+#     # Bind the socket to a specific network interface and port number
+#     s.bind(('147.182.204.67', 5000))
 
-    # Listen for incoming connections
-    s.listen(5)
+#     # Listen for incoming connections
+#     s.listen(5)
 
-    counter = 100
-    print("hello world! we are now starting a TCP server!")
-    while (True):
-        print("this is the loop")
-        counter -= 1
-        # Accept an incoming connection
-        connection, client_address = s.accept()
-        print(client_address)
-        # Send and receive data
-        connection.send('Hello, world!'.encode())
-        data = connection.recv(1024)
-        print(data.decode())
+#     counter = 100
+#     print("hello world! we are now starting a TCP server!")
+#     while (True):
+#         print("this is the loop")
+#         counter -= 1
+#         # Accept an incoming connection
+#         connection, client_address = s.accept()
+#         print(client_address)
+#         # Send and receive data
+#         connection.send('Hello, world!'.encode())
+#         data = connection.recv(1024)
 
-        # # Close the socket
-        connection.close()
-# t1 = Thread(target=socketThread, args=())
-# t1.start()
+#         # Start a Send thread and a Receive thread
 
-socketThread()
+
+#         print(data.decode())
+
+#         # # Close the socket
+#         connection.close()
+# # t1 = Thread(target=socketThread, args=())
+# # t1.start()
+
+# socketThread()
+
+# Create a UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to a specific address and port
+server_socket.bind(("147.182.204.67", 5000))
+
+# Listen for messages
+while True:
+  # Receive a message from the client
+  data, client_address = server_socket.recvfrom(1024)
+
+  # Print the message
+  print("Received message from {}: {}".format(client_address, data))
+
+  # Send a response back to the client
+  server_socket.sendto("Received!".encode(), client_address)
+
+
 
 # @app.route("/", methods=["POST", "GET"])
 # def main():
